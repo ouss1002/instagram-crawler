ce2d4fce860eeb5572e502d3999f0b41ccb41079
ouss1002 <oboudjeniba@gmail.com>
2021-01-07 20:29:28 +0100

    Add comments for functions

--- utils/downloader.js
+++ utils/downloader.js
@@ -3,12 +3,16 @@ const {writeFile} = require('fs');
 const {promisify} = require('util');
 const writeFilePromise = promisify(writeFile);
 
+// This asynchronous function downloads a file from a given URL
+// and saves it in a given directory
 async function downloadFile(url, outputPath) {
     return fetch(url)
         .then(x => x.arrayBuffer())
         .then(x => writeFilePromise(outputPath, Buffer.from(x)));
 }
 
+// this asynchronous function tries to download a file from a given URL
+// in case of a failure (for n times)
 async function downloadRetry(url, outputPath, n) {
     try {
         return await downloadFile(url, outputPath);
