17a99b5585677a7dabdcf2b6079718a232c0db73
ouss1002 <oboudjeniba@gmail.com>
2021-01-07 20:26:46 +0100

    Remove unused functions

--- utils/organizer.js
+++ utils/organizer.js
@@ -1,22 +1,6 @@
 const fs = require('fs');
 const downloader = require('./downloader');
 
-async function writeProfileDirectory(origin, links, rules) {
-
-    let org = `${rules.result}/${origin}/`;
-
-    for(let link of links) {
-        let id = link.split('/');
-        id = id[id.length - 2];
-
-        fs.mkdir('./' + org + id, { recursive: true }, (err) => {
-            if(err) {
-                console.log(err);
-            }
-        });
-    }
-}
-
 async function writeProfileDirectory2(origin, links, rules) {
 
     let org = `${rules.result}/${origin}/`;
@@ -26,24 +10,6 @@ async function writeProfileDirectory2(origin, links, rules) {
             console.log(err);
         }
     });
-}
-
-async function downloadMediaFromPost(post, rules) {
-    let counter = 1;
-
-    for(let arr of Object.entries(post.media)) {
-        if(arr[1] == 'image') {
-            await downloader.downloadRetry(arr[0], `.${rules.result}/${post.profile_id}/${post.post_id}/${counter}.jpg`, 5);
-        }
-        else if(arr[1] == 'video') {
-            await downloader.downloadRetry(arr[0], `.${rules.result}/${post.profile_id}/${post.post_id}/${counter}.mp4`, 5);
-        }
-        else {
-            console.log('ERROR: check file type...');
-        }
-
-        counter++;
-    }
 }
 
 async function downloadMediaFromPost2(post, rules) {
